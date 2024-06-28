fe07564add44682ed4b5cbb9dcfe0725e13a8472
kaka <zengjiahuizjh@gmail.com>
2024-06-28 21:11:35 +0800

    feat: build the layout

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,5 @@
+function AppNav() {
+  return <div>Nav</div>;
+}
+
+export default AppNav;
---
+++ src/components/Map.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "./Sidebar";
+
+function Map() {
+  return (
+    <div className={StyleSheet.app}>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default Map;
